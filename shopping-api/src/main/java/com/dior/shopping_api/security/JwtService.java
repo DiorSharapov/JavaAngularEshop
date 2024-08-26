2c86deba4fd188da76ebae31cd7f7a36e32f5c9e
JaDior <diorsharapov123@gmail.com>
2024-08-26 15:03:45 -0500

    more progress on security

---
+++ shopping-api/src/main/java/com/dior/shopping_api/security/JwtService.java
@@ -0,0 +1,59 @@
+package com.dior.shopping_api.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+    @Value("${application-dev.security.jwt.expriation}")
+    private jwtExpiration;
+
+    @Value("${application-dev.security.jwt.secret-key}")
+    private String secretKey;
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    private String generateToken(Map<String, Object> claims, UserDetails userDetails) {
+        return buildToken(claims, userDetails, jwtExpiration);
+    }
+
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails,
+            long jwtExpriation
+    ) {
+        var authorities = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt((new Date(System.currentTimeMillis())))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpriation))
+                .claim("authorities", authorities)
+                .signWith(getSignInKey())
+                .compact()
+                ;
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+}
---
+++ shopping-api/src/main/java/com/dior/shopping_api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.dior.shopping_api.security;
+
+import com.dior.shopping_api.user.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User email not found"));
+    }
+}
